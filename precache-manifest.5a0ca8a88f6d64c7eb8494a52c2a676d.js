8422e327205491d65ad4a52a92a4d88d17423ebe
Ryan <ryanmagill97@gmail.com>
2020-05-26 21:47:44 -0500

    Updates

---
+++ precache-manifest.5a0ca8a88f6d64c7eb8494a52c2a676d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373c6cae07832d05579faf453a6577bf",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "67919d8e568568f5ecb2",
+    "url": "/react-portfolio/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1c92d65f3106d37cc765",
+    "url": "/react-portfolio/static/css/main.6c621761.chunk.css"
+  },
+  {
+    "revision": "67919d8e568568f5ecb2",
+    "url": "/react-portfolio/static/js/2.3b84926f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.3b84926f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c92d65f3106d37cc765",
+    "url": "/react-portfolio/static/js/main.99fbabb3.chunk.js"
+  },
+  {
+    "revision": "37db3f26b7c0ea36aed0",
+    "url": "/react-portfolio/static/js/runtime-main.d08fb9a1.js"
+  }
+]);
